ff3dc86eb550dfc9ea17fd32116905f79850a700
Henry-moire <henrymoire@gmail.com>
2024-05-30 02:58:15 +0300

    Updated requirements.txt and refactored app.py and consistent_hash.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 import os
-from consistent_hash import map_request
+# from consistent_hash import map_request
 
 
 app = Flask(__name__)
@@ -8,14 +8,17 @@ app = Flask(__name__)
 # Get the server ID from the environment variable
 server_id = os.environ.get('SERVER_ID', 'Unknown')
 
+
 @app.route('/home', methods=['GET'])
 def home():
     message = f"Hello from Server: {server_id}"
     return jsonify({"message": message, "status": "successful"}), 200
 
+
 @app.route('/heartbeat', methods=['GET'])
 def heartbeat():
     return '', 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- consistent_hash.py
+++ consistent_hash.py
@@ -1,28 +1,32 @@
-#step 1
+# step 1
 N = 3  # Number of server containers
 num_slots = 512  # Total number of slots in the consistent hash map
 K = 9  # Number of virtual servers for each server container
+
 
 # Hash function for request mapping
 def hash_request(i):
     return (i + 2*i**2 + 17) % num_slots
 
-# Hash function for virtual server mapping
-def hash_virtual_server(i, j):
-    return (i + j + 2*j**2 + 25) % num_slots
 
-#Step 2
+# Hash function for virtual server mapping
+def hash_virtual_server(i, h):
+    return (i + h + 2*h**2 + 25) % num_slots
+
+
+# Step 2
 consistent_hash_map = {}
 
 # Initialize the consistent hash map
 for server_id in range(N):
     for j in range(K):
-        virtual_server_id = hash_virtual_server(server_id, j)
-        consistent_hash_map[virtual_server_id] = server_id
+        the_virtual_server_id = hash_virtual_server(server_id, j)
+        consistent_hash_map[the_virtual_server_id] = server_id
 
-#Step 3
-def map_request(request_id):
-    hashed_request = hash_request(request_id)
+
+# Step 3
+def map_request(requested_id):
+    hashed_request = hash_request(requested_id)
     
     # Find the next virtual server ID in the consistent hash map
     next_virtual_server_id = None
@@ -36,11 +40,12 @@ def map_request(request_id):
         next_virtual_server_id = sorted(consistent_hash_map.keys())[0]
     
     # Get the server container ID corresponding to the virtual server ID
-    server_id = consistent_hash_map[next_virtual_server_id]
+    the_server_id = consistent_hash_map[next_virtual_server_id]
     
-    return server_id
+    return the_server_id
 
-#Step 4
+
+# Step 4
 request_id = 123
 server_id = map_request(request_id)
 print(f"Request {request_id} is mapped to Server: {server_id}")
